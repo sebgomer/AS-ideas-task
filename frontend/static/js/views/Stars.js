c8decf46a9c2482434ca879478c73c7617259a41
sgomer <sebastian.gomer@axelspringer.com>
2020-10-07 16:23:59 +0200

    beautifying css, errorPage easter egg

--- frontend/static/js/views/Stars.js
+++ frontend/static/js/views/Stars.js
@@ -9,18 +9,21 @@ export default class extends AbstractView {
     // Grab server-side data
     async getHtml() {
         return `
-        <h1>Our currently logged stars</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID<th>
-                    <th>Name<th>
-                    <th>Color<th>
-                </tr>
-            </thead>
-            <tbody id="starTable">
-            </tbody>
-        </table>
+
+        <div class="contentWrapper" >
+            <h1>Our currently logged stars</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID<th>
+                        <th>Name<th>
+                        <th>Color<th>
+                    </tr>
+                </thead>
+                <tbody id="starTable">
+                </tbody>
+            </table>
+        </div>
         `;
     }
 }
--- frontend/static/js/views/Universes.js
+++ frontend/static/js/views/Universes.js
@@ -9,28 +9,30 @@ export default class extends AbstractView {
     // Grab server-side data
     async getHtml() {
         return `
-        <h1>Our currently logged universes</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID<th>
-                    <th>Name<th>
-                    <th>MaxSize<th>
-                </tr>
-            </thead>
-            <tbody id="universeTable">
-            </tbody>
-        </table>
-        <form class="form" id="universeForm"> 
-            <label for="id">Universe ID</label>
-            <input type="text" name="id" id="id"></input>
-            <label for="maxSize">Universe Max Size</label>
-            <input type="text" name="maxSize" id="maxSize"></input>
-            <label for="name">Universe Name</label>
-            <input type="text" name="name" id="name"></input>
-            <button type="submit">Submit</button>
-        </form>
+
+        <div class="contentWrapper" >
+            <h1>Our currently logged universes</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID<th>
+                        <th>Name<th>
+                        <th>MaxSize<th>
+                    </tr>
+                </thead>
+                <tbody id="universeTable">
+                </tbody>
+            </table>
+            <!-- <form class="form" id="universeForm"> 
+                <label for="id">Universe ID</label>
+                <input type="text" name="id" id="id"></input>
+                <label for="maxSize">Universe Max Size</label>
+                <input type="text" name="maxSize" id="maxSize"></input>
+                <label for="name">Universe Name</label>
+                <input type="text" name="name" id="name"></input>
+                <button type="submit">Submit</button>
+            </form> -->
+        </div>
         `;
     }
 }
-
